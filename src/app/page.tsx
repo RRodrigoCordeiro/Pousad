50052d73b44945fbf2eb7851c72f003b84a611ef
RRodrigoCordeiro <rodrigorctavares@gmail.com>
2024-05-14 16:51:51 -0300

    Implementando cards dos quartos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,8 @@ export default function Home() {
         <div className="container mx-auto flex justify-between items-center">
           <div className="flex items-center">
             <Image src="" alt="Pousada Logo" width={100} height={20} />
-            <h1 className="text-2xl font-bold ml-2 text-blue-500">Pousada da Montanha</h1>
+            <h1 className="text-2xl font-bold ml-2 text-black-500">Pousada</h1>
+            <p className="text-2xl font-bold ml-2 text-blue-500">da Montanha</p>
           </div>
           <nav>
             <ul className="flex space-x-4 mr-5">
@@ -22,13 +23,19 @@ export default function Home() {
         </div>
       </header>
       <div className="relative">
-          <Image src="/mar.png" alt="Pousada Logo" width={1519} height={600} />
-          <h1 className="text-3xl absolute top-44 left-1/2 transform -translate-x-1/2 text-white  font-bold">Pousada da Montanha</h1>
-          <p className="text-2xl absolute top-1/2 left-1/2 transform -translate-x-1/2 translate-y-1/2 text-white">Venha conhecer a melhor pousada da Região</p>
+          <Image className="w-[50] h-[625px]" src="/mar.png" alt="Pousada Logo" width={1519} height={800} />
+          <h1 className="text-3xl absolute top-1/2 left-1/2 transform -translate-x-1/2 text-white  font-bold">Pousada da Montanha</h1>
+          <p className="text-2xl absolute top-1/2 mt-14 left-1/2 transform -translate-x-1/2 translate-y-1/2 text-white">Venha conhecer a melhor pousada da Região</p>
+      </div>
+      <div className="text-3xl font-bold text-center mt-24 text-black-400">
+        <h2>SEJA BEM-VINDO(A)!</h2>
+      </div>
+      <div className="text-center mt-18 text-black-400 text-1xl ">
+          <p className="mt-5">Relaxe em nossas acomodações e curta o melhor da nossa capital</p>
+          <p className="mt-5">Temos diversos quartos com o máximo de conforto</p>
       </div>
       <div>
-        <h2>Nossos Quartos</h2>
-     
+        <h2>Conheça nossos quartos</h2>
       </div>
     </main>
   );
