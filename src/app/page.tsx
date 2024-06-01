434b474858c8f7b358b622b8c15494906d5d652a
RRodrigoCordeiro <rodrigorctavares@gmail.com>
2024-05-31 22:02:32 -0300

    Criação da página para fazer a reserva e ajuste no footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 
 
 
+
 export default function Home() {
   return (
     <main>
@@ -128,11 +129,10 @@ export default function Home() {
       <footer className="bg-green-800 h-28 mt-20 flex justify-center items-center text-white" id="contato">
         <div className="flex space-x-20">
           <p className="text-white font-bold max-w-44">Entre em contato e faça seu cadastro:</p>
-          <p className="text-white">Endereço: Av.1234</p>
+          <p className="text-white">Endereço: Av.1234, bairro Cabo Branco</p>
           <p className="text-white">Telefone: (00) 0000-0000</p>
-          <p className="text-white font-bold cursor-pointer hover:text-blue-500">Clique aqui para fazer a reserva</p>
+          <p className="text-white font-bold cursor-pointer hover:text-blue-500"><a href="#reserva.tsx">Clique aqui para fazer a reserva</a></p>
         </div>
-       
       </footer>
 
     </main>
---
+++ src/app/reserva.tsx
@@ -0,0 +1,13 @@
+// pages/SegundaPagina.js
+
+import React from 'react';
+
+const reserva = () => {
+  return (
+    <div>
+      <h1>Esta é a segunda página</h1>
+    </div>
+  );
+};
+
+export default reserva;
