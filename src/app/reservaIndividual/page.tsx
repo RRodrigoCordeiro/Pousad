56e9d31b06bc83dd1675a3a28e844df58da53be8
RRodrigoCordeiro <rodrigorctavares@gmail.com>
2024-08-02 10:45:12 -0300

    Implementação da funcionalidade de seleção de adultos e crianças, com contador de hóspedes e validação do limite máximo de ocupação. Adição do campo para inserção de CEP do usuário.

--- src/app/reservaIndividual/page.tsx
+++ src/app/reservaIndividual/page.tsx
@@ -1,13 +1,15 @@
 "use client"
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import CabecalhoPaginacao from '../components/CabecalhoPaginacao'
 import Background from '../components/Background'
 import Rodape from '../components/Rodape'
 import Calendario from '../components/Calendario'
+import CepApi from '../components/CepApi';
 
 const page = () => {
     const [adulto, setAdulto] = useState(1)
     const [crianca, setCrianca] = useState(0)
+    const [total, setTotal] = useState(0)
 
     const AdicionarAdulto = () => {
         if(adulto < 2){
@@ -27,13 +29,23 @@ const page = () => {
     }
 
     const AdicionarCrianca = () =>{
-        setCrianca(crianca + 1);    
-    }
+        if(crianca < 2){
+            setCrianca(crianca + 1);   
 
-    const RemoverCrianca = () => {
-        setCrianca(crianca - 1);
+        } else{
+            console.log("Erro: O quarto não suporta mias que 02 crianças")
+        }
+         
     }
-    
+    const RemoverCrianca = () => {
+        if(crianca > 0){
+            setCrianca(crianca - 1);
+
+        }
+    }
+    useEffect(() => {
+        setTotal(crianca + adulto)
+    });
   return (
 
       
@@ -57,9 +69,21 @@ const page = () => {
         {
             adulto >= 2 && <p className="text-center mt-1 font-bold text-red-500">Atenção: A capacidade máxima para esse quarto é de até 02 (dois) adultos</p>
         }
-
-
-
+         <fieldset className="flex flex-row items-center justify-evenly space-x-8 m-auto w-[620px] h-11 border border-zinc-500 mt-8">
+          <h3 className="font-bold text-blue-500">Criança(s)</h3>
+          <button onClick={AdicionarCrianca} className="border border-zinc-950 rounded-full h-7 w-7 mt-1 flex items-center justify-center">+</button>
+          <p>{crianca}</p>
+          <button onClick={RemoverCrianca} className="border border-zinc-950 rounded-full  h-7 w-7 mt-1 flex items-center justify-center">-</button>
+        </fieldset>
+        {
+            crianca >=2 && <p className="text-center mt-1 font-bold text-red-500">Atenção: A capacidade máxima para esse quarto é de até 02(duas) crianças.</p>
+        }
+        <fieldset className="flex flex-row items-center justify-evenly space-x-8 m-auto w-[620px] h-11 border border-zinc-500 mt-8">
+          <h3 className="font-bold text-blue-500">Total de hóspedes:</h3>
+          <p>{total}</p>
+        </fieldset>
+        <h3 className='font-bold mt-10 ml-16'>Complete as informações abaixo:</h3>
+        <CepApi/>
         </fieldset>
 
 
