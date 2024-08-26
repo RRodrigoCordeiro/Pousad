30ae08b70a097395efc045b46054531072d92971
RRodrigoCordeiro <rodrigorctavares@gmail.com>
2024-08-26 17:16:24 -0300

    Adicioando os feedbacks no carrossel

--- src/app/components/Carrosel.tsx
+++ src/app/components/Carrosel.tsx
@@ -4,10 +4,10 @@ import { motion } from 'framer-motion'
 import Image from 'next/image'
 
 
-import Image1 from '../../../public/mar.png'
-import Image2 from '../../../public/QuartoCasal.jpg'
-import Image3 from '../../../public/QuartoTematico.jpg'
-import Image4 from '../../../public/seta.png'
+import Image1 from '../../../public/depoimentoCarla.png'
+import Image2 from '../../../public/depoimentoFelipe.png'
+import Image3 from '../../../public/depoimentoPedro.png'
+import Image4 from '../../../public/depoimentorRafael.png'
 
 const images = [Image1, Image2, Image3, Image4]
 
@@ -24,17 +24,17 @@ const Carrosel = () => {
    
 
   return (
-    <div className="w-full mx-auto min-h-screen flex items-center justify-center max-w-[900px]">
+    <div className="w-full mx-auto h-[50vh]   mb-1 flex items-center justify-center max-w-[900px]">
         <motion.div ref={carousel}  className="cursor-grab overflow-hidden" whileTap={{cursor: "grabbing" }}>
             <motion.div className='flex' 
             drag="x"
             dragConstraints={{ right: 0, left: -width }}
             >
-                {images.map(image => (
+                 {images.map(image => (
                     <motion.div className='min-h-[200px] min-w-[400px] p-3.5' key={image}>
-                         <Image className="w-full h-[90%] rounded-[12px] pointer-events-none " src={image} alt="Pousada Logo" width={200} height={80} />
+                         <Image className="w-full h-[50%] rounded-[12px] pointer-events-none " src={image} alt="Pousada Logo" width={200} height={80} />
                     </motion.div>
-                ))}
+                ))} 
             </motion.div>
         </motion.div>
     </div>
