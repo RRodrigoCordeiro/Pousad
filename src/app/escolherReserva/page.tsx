764463384b4627958e4f0aa8bd8d8544c4500e99
RRodrigoCordeiro <rodrigorctavares@gmail.com>
2024-08-29 17:02:52 -0300

    responsividade da página de escolher o  quarto da pousada

--- src/app/escolherReserva/page.tsx
+++ src/app/escolherReserva/page.tsx
@@ -6,30 +6,34 @@ import Background from '../components/Background'
 import Rodape from '../components/Rodape'
 
 const page = () => {
+    // flex flex-row justify-center space-x-44
     return (
         <>
             <CabecalhoPaginacao />
             <Background />
-            <h1 className="text-center font-bold mt-11 text-2xl">Escolha o quarto ideal para a sua Hospedagem</h1>
-            <h2 className="text-center mt-11 text-green-900  text-2xl">Opções disponíveis no momento:</h2>
-            <div className="flex flex-row justify-center space-x-24">
+            <h1 className="text-center font-bold mt-11 text-1xl lg:text-2xl">Escolha o quarto ideal para a sua Hospedagem</h1>
+            <h2 className="text-center mt-11 text-green-900 text-1xl  lg:text-2xl">Opções disponíveis no momento:</h2>
+            <div className="flex flex-col justify-center items-center lg:flex-row lg:space-x-8">
                 <Link href="/reservaIndividual">
                     <Image
                         src="/QuartoIndividual.jpg"
                         alt="Imagem do Quarto Individual"
                         width={510}
                         height={10}
-                        className=" w-[350px] h-[300px] mt-20 hover:scale-110 duration-300 "
+                        className=" w-[290px] h-[300px] mt-20 hover:scale-110 duration-300 "
                     />
+                    <p className="text-center mt-8"><span className="text-green-900">Capacidade Máxima:</span> 4 pessoas</p>
                 </Link>
+                
                 <Link href="/reserva">
                     <Image
                         src="/QuartoCasal.jpg"
                         alt="Imagem do Quarto Individual"
                         width={510}
                         height={10}
-                        className=" w-[350px] h-[300px] mt-20 hover:scale-110 duration-300 "
+                        className=" w-[290px] h-[300px] mt-20 hover:scale-110 duration-300 "
                     />
+                     <p className="text-center mt-8"><span className="text-green-900">Capacidade Máxima:</span> 8 pessoas</p>
                 </Link>
                 <Link href="/reserva">
                     <Image
@@ -37,14 +41,10 @@ const page = () => {
                         alt="Imagem do Quarto Individual"
                         width={510}
                         height={10}
-                        className=" w-[350px] h-[300px] mt-20 hover:scale-110 duration-300"
+                        className=" w-[290px] h-[300px] mt-20 hover:scale-110 duration-300"
                     />
+                    <p className="text-center mt-8"><span className="text-green-900">Capacidade Máxima:</span> 8 pessoas</p>
                 </Link>
-            </div>
-            <div className="text-center flex flex-row  justify-evenly mt-10">
-                <p><span className="text-green-900">Capacidade Máxima:</span> 4 pessoas</p>
-                <p><span className="text-green-900">Capacidae Máxima:</span> 8 pessoas</p>
-                <p><span className="text-green-900">Capacidade Máxima:</span> 8 pessoas</p>
             </div>
             <Rodape />
         </>
