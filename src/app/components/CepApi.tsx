4e93c5c09388664cd6737c754c177ba1bdc0bb15
RRodrigoCordeiro <rodrigorctavares@gmail.com>
2024-09-03 17:52:09 -0300

    responsividade da página de reserva para os quartos individuais

--- src/app/components/CepApi.tsx
+++ src/app/components/CepApi.tsx
@@ -57,27 +57,27 @@ const CepApi = () => {
     };
     return (
         <>
-            <div className='mt-6 mb-12 flex justify-center space-x-9'>
+            <div className='mt-6 mb-12 flex justify-center space-x-3  lg:space-x-9 '>
                 <button onClick={buscarCep}>Clicar para buscar CEP:</button>
                 <input
                     type="text"
                     value={cep}
                     onChange={(e) => setCep(e.target.value)}
                     placeholder='Digite seu CEP'
-                    className='border border-zinc-600 '
+                    className='border border-zinc-600 w-32 lg:w-[220px] text-center '
                 />
             </div>
             {error && <p className='text-red-600 text-center font-bold'>{error}</p>}
-            <div className='flex flex-row justify-evenly mt-11'>
-                <div>
+            <div className='flex flex-col items-center lg:flex-row justify-evenly mt-11'>
+                <div className="flex sm:flex-row space-x-7  lg:mt-0 lg:space-x-0 lg:flex-col">
                     <p className="font-bold text-blue-500">Endereço:</p>
                     <span>{endereco?.cep}</span>
                 </div>
-                <div>
+                <div  className="flex sm:flex-row mt-10 space-x-7  lg:mt-0 lg:space-x-0 lg:flex-col">
                     <p className="font-bold text-blue-500">Logradouro:</p>
                     <span>{endereco?.logradouro} </span>
                 </div>
-                <div>
+                <div className="flex flex-row space-x-3  mt-10 lg:mt-0 lg:flex-col lg:space-x-0 ">
                     <p className="font-bold text-blue-500">Complemento:</p>
                     <input
                         type="text"
--- src/app/reservaIndividual/page.tsx
+++ src/app/reservaIndividual/page.tsx
@@ -75,10 +75,10 @@ const page = () => {
         <CabecalhoPaginacao/>
         <Background/>
 
-        <fieldset className="border border-blue-500 rounded-lg p-4 mt-10 container mx-auto h-full">
+        <fieldset className="border border-blue-500 rounded-lg p-4 mt-10 container md:w-[750px] lg:w-[1000px] w-auto mx-auto h-full">
         <legend className="font-bold">POUSADA DA <span className="text-blue-500 font-bold">MONTANHA</span></legend>
         <Calendario/>
-        <fieldset className="flex flex-row items-center justify-evenly space-x-8 m-auto mt-11 border border-zinc-500 w-[620px] h-11">
+        <fieldset className="flex flex-row items-center justify-evenly space-x-8 m-auto mt-11 border border-zinc-500 w-[300px] lg:w-[620px] h-11">
           <h3 className="font-bold text-blue-500">Adulto(s)</h3>
           <button onClick={AdicionarAdulto} className="border border-zinc-950 rounded-full h-7 w-7 mt-1 flex items-center justify-center">+</button>
           <p>{adulto}</p>
@@ -90,7 +90,7 @@ const page = () => {
         {
             adulto >= 2 && <p className="text-center mt-1 font-bold text-red-500">Atenção: A capacidade máxima para esse quarto é de até 02 (dois) adultos</p>
         }
-         <fieldset className="flex flex-row items-center justify-evenly space-x-8 m-auto w-[620px] h-11 border border-zinc-500 mt-8">
+         <fieldset className="flex flex-row items-center justify-evenly space-x-8 m-auto w-[300px] lg:w-[620px] h-11 border border-zinc-500 mt-8">
           <h3 className="font-bold text-blue-500">Criança(s)</h3>
           <button onClick={AdicionarCrianca} className="border border-zinc-950 rounded-full h-7 w-7 mt-1 flex items-center justify-center">+</button>
           <p>{crianca}</p>
@@ -99,7 +99,7 @@ const page = () => {
         {
             crianca >=2 && <p className="text-center mt-1 font-bold text-red-500">Atenção: A capacidade máxima para esse quarto é de até 02(duas) crianças.</p>
         }
-        <fieldset className="flex flex-row items-center justify-evenly space-x-8 m-auto w-[620px] h-11 border border-zinc-500 mt-8">
+        <fieldset className="flex flex-row items-center justify-evenly space-x-8 m-auto w-[300px] lg:w-[620px] h-11 border border-zinc-500 mt-8">
           <h3 className="font-bold text-blue-500">Total de hóspedes:</h3>
           <p>{total}</p>
         </fieldset>
